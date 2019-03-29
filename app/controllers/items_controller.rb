46bb36eb645ed5ca19c1ee92503469a8f7de2e53
yotuba088 <k44.ysmr@gmail.com>
2019-03-29 14:25:29 +0900

    sub 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,24 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   def create
   end
 
-  def new
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
   end
 end
