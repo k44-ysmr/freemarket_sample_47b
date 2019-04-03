10d40e89f745dac716563a22a9b043c58cbf4d72
subaring <subasuba>
2019-04-03 20:33:54 +0900

    Identify-view
    # WHAT
    ユーザー本人確認ページを実装した
    # WHY
    必要な機能なため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def index
   end
-  
+
   def show
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def identify
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     }
 
   root to: 'items#index'
-  resources :users
+  resources :users do
+    get 'identify'
+  end
   resources :items
   resources :buys
 end
