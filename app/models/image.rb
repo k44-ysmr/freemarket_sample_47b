0baa23efb6393c7cad8d04de39c0082dbeab58db
shiratori-sh <janpaku.games@gmail.com>
2019-04-11 13:08:23 +0900

    Added
    画像表示,価格を動的に表示するためのjsファイル追加。
    Viewファイル修正。

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,9 @@
 class Image < ApplicationRecord
-  belongs_to :item, inverse_of: :images
+  belongs_to :item, optional: true, inverse_of: :images
 
   mount_uploader :name, ImageUploader
 
-  validates :name, presence: true
+
+  # validates :name, presence: true
   validates :item, presence: true
 end
