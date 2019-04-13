52507557f69353c4a914a804fe664de0c84eaee1
shiratori-sh <janpaku.games@gmail.com>
2019-04-13 15:21:36 +0900

    edit
    商品購入機能実装
    trade_status、status
    1 = 出品中
    2 = 停止中
    3 = 取引中
    4 = 売却済　
    として実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,10 +1,36 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [ :index, :create]
+  before_action :set_buys, only: [ :index, :create]
   def index
+    # @item = Item.find(params[:item_id])
   end
 
   def create
+    if @item.trade_status == 1
+      create_purchase_history(@item)
+      # 本家の購入確認後のページの確認ができないため仮置きでrootページを表示
+      redirect_to root_path
+    else
+      # 本家の購入確認後のページの確認ができないため仮置きで商品購入ページを再表示
+      redirect_to item_buys_path
+    end
   end
 
   def show
   end
+
+private
+
+  def set_buys
+    @item = Item.find(params[:item_id])
+    @images = @item.images
+    # @user = User.where(id: current_user)
+  end
+
+  def create_purchase_history(item)
+    partner = TradingPartner.where(seller_id: item.user_id, buyer_id: current_user.id).first_or_create
+    Order.create(item_id: item.id, trading_partner_id: partner.id, status: 3)
+    item.update(trade_status: 3)
+  end
+
 end
