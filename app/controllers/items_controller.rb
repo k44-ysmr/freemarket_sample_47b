254361dea064ee19bb31390d95668bfcca5eea94
shiratori-sh <janpaku.games@gmail.com>
2019-04-13 15:11:59 +0900

    Fixed
    不要パラメーター削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,8 @@ class ItemsController < ApplicationController
   end
 
   private
+
     def item_params
-      params.require(:item).permit(:name, :description,:price,:condition,:shipping_fee,:shipping_from,:days_before_shipping,:shipping_method,:trade_status,:prefecture_id,:brand_id,:category_id,:size_id,images_attributes: [:id,:name]).merge(user_id: current_user.id)
+      params.require(:item).permit(:name, :description,:price,:condition,:shipping_fee,:days_before_shipping,:shipping_method,:trade_status,:prefecture_id,:brand_id,:category_id,:size_id,images_attributes: [:id,:name]).merge(user_id: current_user.id)
     end
 end
