061111d9226b970b784ad8d15b2bf5a011105d2a
shiratori-sh <janpaku.games@gmail.com>
2019-04-13 15:12:54 +0900

    fixed
    ルーティング見直し

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   resources :users do
     get 'profile'
   end
-  resources :items
-  resources :buys
+  resources :items do
+    resources :buys
+  end
   resources :credit_card
 
   resources :identifications, only: [:index]
