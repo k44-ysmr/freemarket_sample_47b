31cf621cbc0ddde95447097e58fadaf6af12bc83
shiratori-sh <janpaku.games@gmail.com>
2019-04-17 20:48:30 +0900

    edit
    ・Pay.jpでクレジットカード決済をできるように編集
    ・出品者と購入者が同じ場合直接pathで購入ページを表示せず、rootページを表示するようにbefor_action定義

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,25 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!, only: [ :index, :create]
   before_action :set_buys, only: [ :index, :create]
+  before_action :validation_seller_and_buyer_prevent_the_same, only: [:index, :create]
 
   def index
   end
 
   def create
     if @item.trade_status == 1
-      create_purchase_history(@item)
-      # 本家の購入確認後のページの確認ができないため仮置きでrootページを表示
-      redirect_to root_path
+      if @credit_card&.customer_id
+          Payjp.api_key = PAYJP_SECRET_KEY
+          customer_id =@credit_card.customer_id
+          Payjp::Charge.create(currency: 'jpy', amount: @item.price, customer: customer_id)
+          create_purchase_history(@item)
+          # 本家の購入確認後のページの確認ができないため仮置きでrootページを表示
+          redirect_to root_path
+      else
+          create_purchase_history(@item)
+          # 本家の購入確認後のページの確認ができないため仮置きでrootページを表示
+          redirect_to root_path
+      end
     else
       # 本家の購入確認後のページの確認ができないため仮置きで商品購入ページを再表示
       redirect_to item_buys_path
@@ -24,12 +34,19 @@ private
   def set_buys
     @item = Item.find(params[:item_id])
     @images = @item.images
+    @credit_card = CreditCard.find_by(user_id: current_user.id)
   end
 
   def create_purchase_history(item)
     partner = TradingPartner.where(seller_id: item.user_id, buyer_id: current_user.id).first_or_create
     Order.create(item_id: item.id, trading_partner_id: partner.id, status: 3)
     item.update(trade_status: 3)
+  end
+
+  def validation_seller_and_buyer_prevent_the_same
+    if current_user.id == @item.user_id
+      redirect_to new_user_session_path
+    end
   end
 
 end
