22661d5970d83d905e9a7d7ca5162a7e8093831c
shiratori-sh <janpaku.games@gmail.com>
2019-04-23 14:47:42 +0900

    edit
    Validation-item

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,35 @@ class Item < ApplicationRecord
 
   enum days_before_shipping: { two_days: 1, four_days: 2, seven_days: 3}
 
+  validates :name,
+    presence: true,
+    length: { maximum: 40 }
+  validates :price,
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "販売価格は300以上9,999,999以内で入力してください" }
+  validates :description,
+    presence: true,
+    length: { maximum: 1000 }
+  validates :condition,
+    presence: { message: "選択して下さい" }
+  validates :shipping_fee,
+    presence: { message: "選択して下さい" }
+  validates :prefecture_id,
+    presence: { message: "選択して下さい" }
+  validates :days_before_shipping,
+    presence: { message: "選択して下さい" }
+  validates :shipping_method,
+    presence: { message: "選択して下さい" }
+  validates :trade_status,
+    presence: true
+  validates :brand,
+    length: { maximum: 40 }
+  validates :category_id,
+    numericality: { greater_than: 0, message: "選択して下さい" }
+  validates :user_id,
+    presence: true
+  validates :size_id,
+    numericality: { greater_than: 0, message: "選択して下さい" }
+
   def self.set_index(id)
     Item.where(id).limit(4).order("created_at DESC")
   end
